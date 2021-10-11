931908eb818b5c55ee91aa3a38469c783bd78f45
Nelson Kiprono <nelsohnkiprono@gmail.com>
2021-10-11 12:00:28 +0000

    customers.php edited online with Bitbucket

--- objects/customers.php
+++ objects/customers.php
@@ -144,7 +144,8 @@ AND NOT EXISTS
         global $conn;
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        $response = "";
+        //$response = "";
+        $response =array("success" => "2", "message" => "nothing yet");
             
             try {
                 
