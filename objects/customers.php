d3946243ed160e0ffe316e69a4a8877035d871e9
Nelson Kiprono <nelsohnkiprono@gmail.com>
2022-03-28 15:09:33 +0000

    customers.php edited online with Bitbucket

--- objects/customers.php
+++ objects/customers.php
@@ -125,7 +125,7 @@ AND NOT EXISTS
   (SELECT *
    FROM   sma_tickets
    WHERE  sma_shops.id = sma_tickets.shop_id and sma_tickets.date = CURRENT_DATE and sma_tickets.created_at < ?) and 
-   sma_vehicles.id = ? and ((sma_customers.active = 0 and sma_allocation_days.disabled_date != ?) or sma_customers.active = 1 ) and sma_allocation_days.day = ? and sma_vehicle_route.day = ? and 
+   sma_vehicles.id = ? and sma_customers.active = 1 and ((sma_allocation_days.active = 0 and sma_allocation_days.disabled_date != ?) or sma_allocation_days.active = 1 ) and sma_allocation_days.day = ? and sma_vehicle_route.day = ? and 
    sma_allocation_days.expiry IS NULL or sma_allocation_days.expiry <= CURRENT_TIMESTAMP GROUP BY sma_shops.id ORDER BY sma_allocation_days.position ASC";
 
             $current_date = date("Y-m-d").' '.'23:59:00';
